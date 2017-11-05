472ac75dd59a42846996b5d9ccd4a0cd07f642f0
Ethan Stewart <ethan@atdllc.com>
2017-11-05 01:08:54 -0400

    updates to mho-types.h

--- mho-types.h
+++ mho-types.h
@@ -7,9 +7,11 @@
 namespace mho {
 
 typedef uint32_t node_id_t;
+typedef uint32_t device_id_t;
 typedef uint32_t type_id_t;
 
 typedef double value_t;
+typedef double calibration_t;
 
 // use struct timespec for times.
 // { .tv_sec, .tv_nsec }
