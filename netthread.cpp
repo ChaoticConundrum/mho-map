eaaa8338a06b8aa0a473d12f0a648fd20175dd3f
Charlie Waters <cawiii@me.com>
2017-11-05 00:21:46 -0400

    add catches, c++11

--- netthread.cpp
+++ netthread.cpp
@@ -11,7 +11,6 @@ void sendData(ZPointer<Connection> conn, const ZBinary &data){
     // Send data
     conn->out_buffer.concat(data);
 
-    LOG("Out: " << conn->out_buffer.size() << " bytes");
     //RLOG(conn->out_buffer.dumpBytes(1, 32));
 
     conn->final = true;
@@ -24,6 +23,10 @@ void sendData(ZPointer<Connection> conn, const ZBinary &data){
     if(ret != 8){
         ELOG("write error");
     }
+}
+
+void sendJSON(ZPointer<Connection> conn, ZJSON json){
+    sendData(conn, json.encode() + "\r\n");
 }
 
 NetThread::NetThread(){
@@ -47,30 +50,62 @@ void *NetThread::run(void *arg){
 
         conn->lock.lock();
 
-        switch(conn->status){
-            case Connection::READ: {
-                LOG("In: " << conn->in_buffer.size() << " bytes");
-                ZString str(conn->in_buffer.raw(), conn->in_buffer.size());
-                ZJSON json;
+        try {
 
-                if(!json.decode(str)){
-                    sendData(conn, ZString("{ \"error\" : \"invalid json\" }"));
+            switch(conn->status){
+                case Connection::READ: {
+                    //RLOG(conn->in_buffer.dumpBytes(1, 32));
+
+                    ZString str(conn->in_buffer.raw(), conn->in_buffer.size());
+                    str.strip('\n');
+                    str.strip('\r');
+                    str.strip('\n');
+
+                    if(str.size() == 0){
+                        conn->in_buffer.clear();
+                        break;
+                    }
+
+                    ZJSON json;
+                    if(!json.decode(str) || json.type() != ZJSON::OBJECT){
+                        ELOG("invalid request json");
+                        conn->in_buffer.clear();
+                        ZJSON resp(ZJSON::OBJECT);
+                        resp["error"] = "invalid json";
+                        sendJSON(conn, resp);
+                        break;
+                    }
+                    conn->in_buffer.clear();
+
+                    if(!json.object().contains("func") || json["func"].type() != ZJSON::STRING){
+                        ELOG("invalid request fields");
+                        ZJSON resp(ZJSON::OBJECT);
+                        resp["error"] = "invalid fields";
+                        sendJSON(conn, resp);
+                        break;
+                    }
+
+                    ZString func = json["func"].string();
+                    LOG(str);
+                    LOG("Function: " << func);
+
+                    ZJSON resp(ZJSON::OBJECT);
+                    resp["seq"] = 0;
+                    sendJSON(conn, resp);
                     break;
                 }
-                conn->in_buffer.clear();
 
-                LOG(str);
-                sendData(conn, str);
-                break;
+                case Connection::WRITE:
+                    break;
+
+                default:
+                   ELOG("Bad connection status");
+                   conn->connection->close();
+                   break;
             }
 
-            case Connection::WRITE:
-                break;
-
-            default:
-                ELOG("Bad connection status");
-                conn->connection->close();
-                break;
+        } catch(ZException e){
+            ELOG("EXCEPTION: " << e.what());
         }
 
         conn->lock.unlock();
