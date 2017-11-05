b9f32bcef828f696ae35b46dc2e9cc847e6a4c3d
Ethan Stewart <ethan@atdllc.com>
2017-11-04 21:53:43 -0400

    Added mho-types.h

---
+++ mho-types.h
@@ -0,0 +1,16 @@
+#ifndef MHO_TYPES_H_INCLUDED
+#define MHO_TYPES_H_INCLUDED
+
+// I love bloat!
+#include <stdint.h>
+
+namespace mho {
+
+typedef uint32_t node_id_t;
+typedef uint32_t type_id_t;
+
+typedef double value_t:
+
+}; // mho
+
+#endif /* MHO_TYPES_H_INCLUDED */
