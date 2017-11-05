01d6747e19c7c59a67ca746970eb8c6deda6feea
Charlie Waters <cawiii@me.com>
2017-11-05 04:19:46 -0500

    libchaos bugfix, add some db function declarations

--- mho-types.h
+++ mho-types.h
@@ -1,21 +1,59 @@
 #ifndef MHO_TYPES_H_INCLUDED
 #define MHO_TYPES_H_INCLUDED
 
-// I love bloat!
 #include <stdint.h>
+#include <time.h>
+#include <string>
+
+using namespace std;
 
 namespace mho {
 
-typedef uint32_t node_id_t;
-typedef uint32_t device_id_t;
 typedef uint32_t driver_id_t;
+typedef uint32_t device_id_t;
 typedef uint64_t reading_id_t;
+typedef uint32_t node_id_t;
 
 typedef double value_t;
-typedef double calibration_t;
 
-// use struct timespec for times.
-// { .tv_sec, .tv_nsec }
+enum device_state {
+    INACTIVE = 0,
+    ACTIVE = 1,
+    DELETED = 2,
+};
+
+struct driver_info {
+    driver_id_t type_id;
+    string name;
+    string description;
+};
+
+struct device_info {
+    device_id_t device_id;
+    driver_id_t type_id;
+    string description;
+    node_id_t node_id;
+    value_t calibration;
+    string address;
+    device_state state;
+};
+
+struct reading_info {
+    reading_id_t reading_id;
+    node_id_t node_id;
+    value_t raw_value;
+    value_t adj_value;
+    struct timespec time;
+};
+
+struct node_info {
+    node_id_t node_id;
+    node_id_t parent_id;
+    string name;
+    string description;
+    uint32_t time_added;
+    uint32_t time_removed;
+};
 
 } // mho
 
